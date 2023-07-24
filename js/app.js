66a0bca13db83fda8f3f7b9ce4a3395b9a98bec3
sahan <hyanjusahan@gmail.com>
2023-07-24 11:41:29 +0545

    js var, data types, operators

---
+++ js/app.js
@@ -0,0 +1,39 @@
+console.log(c);
+var c = 5221312;
+
+console.log(2 + 1);
+const d = 5;
+console.log(d);
+
+let a = 1;
+console.log(a);
+
+
+const add = null;
+console.log(add);
+console.log(typeof add);
+
+let details = { name: "ram",age:5,isActive:true };
+console.log(details);
+
+console.log(a/d);
+
+a += d;
+console.log(a);
+let b = 1;
+let e = 1;
+console.log(b == e);
+console.log(b == 1);
+console.log(b === e);
+
+let f = 8; let g = 7;
+
+console.log(f>=0);
+if(b==1 && e == 1) {
+    console.log(true);
+}
+else {console.log(false);}
+
+console.log(a==6 ? "true":"false");
+
+console.log(6+ 'asd');
