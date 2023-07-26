b1399c9f80d8e4c9e74cf6a2455e71aa2aada508
sahan <hyanjusahan@gmail.com>
2023-07-26 11:43:30 +0545

    if else switch

--- js/app.js
+++ js/app.js
@@ -8,15 +8,14 @@ console.log(d);
 let a = 1;
 console.log(a);
 
-
 const add = null;
 console.log(add);
 console.log(typeof add);
 
-let details = { name: "ram",age:5,isActive:true };
+let details = { name: "ram", age: 5, isActive: true };
 console.log(details);
 
-console.log(a/d);
+console.log(a / d);
 
 a += d;
 console.log(a);
@@ -26,43 +25,98 @@ console.log(b == e);
 console.log(b == 1);
 console.log(b === e);
 
-let f = 8; let g = 7;
+let f = 8;
+let g = 7;
 
-console.log(f>=0);
-if(b==1 && e == 1) {
-    console.log(true);
+console.log(f >= 0);
+if (b == 1 && e == 1) {
+  console.log(true);
+} else {
+  console.log(false);
 }
-else {console.log(false);}
 
-console.log(a==6 ? "true":"false");
+console.log(a == 6 ? "true" : "false");
 
-console.log(6+ 'aasdsd');
+console.log(6 + "aasdsd");
 console.log(fw);
 var fw;
-fw=7777;
+fw = 7777;
 console.log(fw);
 
-const mm=50;
-const randomNum =Math.floor(Math.random() * 60)+50;
-if(mm >= randomNum){
-    console.log('greater than');
-    console.log(randomNum);
+const mm = 50;
+const randomNum = Math.floor(Math.random() * 60) + 50;
+if (mm >= randomNum) {
+  console.log("greater than");
+  console.log(randomNum);
+} else if (mm == randomNum) {
+  console.log("equal");
+  console.log(randomNum);
+} else {
+  console.log("smaller");
+  console.log(randomNum);
 }
-else if (mm == randomNum){
-    console.log('equal');
-    console.log(randomNum);
-}
-else {
-    console.log('smaller')
-    console.log(randomNum);
-}
-let name =["jeniss","Sahansss","Munassss","Shyamss"];
-const randomName = Math.floor(Math.random() * 5)+1;
-console.log(randomName);
 
-if(randomName <=5 ){
-    console.log(name.filter(randomName));
-}else{
-    console.log(name);
+// let numm = Number(window.prompt("enter a number"));
+let numm = 1;
+switch (numm) {
+  case 1:
+    console.log("yo 1 ho");
+    break;
+  case 2:
+    console.log("yo 2 ho");
+    break;
+  default:
+    console.log("yo default ho");
+    break;
 }
+let name = ["jeniss", "Sahansss", "Munassss", "Shyamss"];
+// const randomName = Math.floor(Math.random() * 5)+1;
+// const resultName = prompt("Enter a Number");
+// const result = name.filter((names) => names.length > resultName);
+// console.log(resultName);
+
+// if (name) {
+// } else {
+//   console.log(name);
+// }
+
+// let numChecker = Number(prompt("enter a number"));
+// if (numChecker > 0){
+//     console.log('positive');
+// }
+// else if (numChecker == 0){
+//     console.log("equal");
+// }
+// else{
+//     console.log('negative');
+// }
+
+
+
+let users = [
+  {
+    name: prompt("enter a name"),
+    age: prompt("Enter age"),
+  }
+];
+users.map(user => {
+    if(user.age >=18){
+        console.log(user);
+   console.log("eligible to vote")
+    }
+    else{
+        console.log(user);
+        console.log("not eligible")
+    }
+
+});
+
+// let year = Number(prompt("enter a number"));
+// if(year % 4 === 0 && year %100 !== 0 || year %400 === 0) {
+//     console.log("leap year");
+// }
+// else{
+//     console.log("not a leap year")
+// }
+
 
