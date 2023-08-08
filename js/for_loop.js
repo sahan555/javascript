49f645a352dbf98f70c3f3dbd56ddc8464f1d2a4
sahan <hyanjusahan@gmail.com>
2023-08-08 11:36:33 +0545

    loop

--- js/for_loop.js
+++ js/for_loop.js
@@ -260,9 +260,9 @@ for (i = 0; i < 100; i++) {
   //   console.log("buzz");
   // } else if (bigNum[i] % 5 == 0) {
   //   console.log("kill");
-  // } 
-  if (bigNum[i] % 3 ==0 && bigNum[i] % 5==0) {
-    console.log("buzzKill",bigNum[i]);
+  // }
+  if (bigNum[i] % 3 == 0 && bigNum[i] % 5 == 0) {
+    console.log("buzzKill", bigNum[i]);
   } else {
     continue;
   }
@@ -270,16 +270,58 @@ for (i = 0; i < 100; i++) {
 multiThree = bigNum.filter((num) => num % 3 == 0);
 console.log(multiThree);
 
-const arrayOfObjects =[
-  {name:'Ramcha',age:44},
- { name:'asda',age:42134},
-  {name:'asd',age:442},
-  {name:'rwqe',age:424}
-]
-const names=arrayOfObjects.map(obj => obj);
+const arrayOfObjects = [
+  { name: "Ramcha", age: 44 },
+  { name: "asda", age: 42134 },
+  { name: "asd", age: 442 },
+  { name: "rwqe", age: 424 },
+];
+const names = arrayOfObjects.map((obj) => obj);
 console.log(names);
 console.log(names[0]);
 console.log(names[0].name);
 console.log(names[0].age);
-const ages=arrayOfObjects.map(obj => obj.age);
+const ages = arrayOfObjects.map((obj) => obj.age);
 console.log(ages);
+
+function mysum(a, b) {
+  return a + b;
+}
+console.log(mysum(18, 5));
+
+// let sumArray = 0;
+// let arraySum = [1, 0, 5, 4, 7, 8, 9, 10];
+// for (i = 0; i < arraySum.length; i++) {
+//   sumArray += arraySum[i];
+// }
+// console.log(sumArray);
+
+function sumArray(arraySum) {
+  let assada=0;
+  for (i = 0; i < arraySum.length; i++) {
+    assada += arraySum[i];
+  }
+  console.log(assada);
+}
+sumArray([1,5,4,8]);
+
+// let arraySum = [1, 0, 5, 4, 7, 8, 9, 10];
+
+// let ranNumber=Math.floor(Math.random() *10);
+// console.log(ranNumber);
+// if(ranNumber % 2 ==0){
+//   console.log("even",ranNumber);
+// }
+// else{
+//   console.log(`odd, ${ranNumber}`);
+// }
+
+function ranNumber(number) {
+  if (number % 2 == 0) {
+    console.log("even", number);
+  } else {
+    console.log("odd", number);
+  }
+}
+const ranChecker = ranNumber(Math.floor(Math.random() * 10));
+
