e78ace491d1c1ca0b79bc792f6d45f6c31081a3b
sahan <hyanjusahan@gmail.com>
2023-08-11 11:56:41 +0545

    form

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("myform");
+const form = document.getElementById("form_id");
 const passwordInput = document.getElementById("password");
 
 const eyeIcon = document.querySelector(".eyeicon");
@@ -15,18 +15,36 @@ eyeIcon.addEventListener("click", function () {
 });
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const name =document.getElementById('name').value;                
-    const email =document.getElementById('email').value;   
-    const password = passwordInput.value;
-    if (name === '') {
-      alert('Please enter your name.');
-      return;
-    };
-    submitForm();
+  e.preventDefault();
+  const name = document.getElementById("name");
+  const email = document.getElementById("email").value;
+  const password = passwordInput.value;
 
+  if (name.value === ''|| name.value == null) {
+    // alert('Please enter your name.');;
+    const newspan = document.createElement("span");
+    name.parentElement.append(newspan);
+    newspan.classList = 'error';
+    newspan.innerHTML = 'please enter';
+
+    return;
+  }
+
+  if (email === '') {
+    alert('Please enter your email.');
+    return;
+  }
+  if (password === '') {
+    alert('Please enter your password.');
+    return;
+  }
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if(!regex.test(password)) {
+    passwordInput.setCustomValidity('Password should be 8 characters long along with one special character, one number and one capital letter');
+    return;
+  }
+  submitForm();
 });
 function submitForm() {
-  // Here, you can submit the form data using AJAX or perform any other actions
-  alert('Form submitted successfully!');
+  alert("Form submitted successfully!");
 }
